0fc7142a07b87f66cf6048b42db8523b24002ff9
naohiro-178 <69035007+naohiro-178@users.noreply.github.com>
2020-08-31 16:13:50 +0900

    ログイン・新規登録への移動
    link_toを用いてトップページからログインと新規登録ページへの移動の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root "items#index"
   get 'delivery/index'
   get 'purchase/index'
   get 'users/index'
-  devise_for :users
-  root "items#index"
   resources :users, only: [:edit, :update]
+  resources :items, only: [:create]
 end
