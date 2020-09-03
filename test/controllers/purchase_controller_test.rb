96450e30d27eb597ec0d438b0877cd809f371c6f
naohiro-178 <69035007+naohiro-178@users.noreply.github.com>
2020-09-03 12:24:22 +0900

    rubocop使用による修正

--- test/controllers/purchase_controller_test.rb
+++ test/controllers/purchase_controller_test.rb
@@ -1,9 +1,8 @@
 require 'test_helper'
 
 class PurchaseControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get purchase_index_url
     assert_response :success
   end
-
 end
