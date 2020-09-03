3320600c832043d44d940355c89d34a08f428756
naohiro-178 <69035007+naohiro-178@users.noreply.github.com>
2020-09-04 06:00:07 +0900

    出品ページ遷移のための設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,25 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
   end
+
+  def show
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
 end
