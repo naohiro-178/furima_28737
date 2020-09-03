81cd9e3e8eb9433e185d0e3dd9ac19288257c906
naohiro-178 <69035007+naohiro-178@users.noreply.github.com>
2020-09-04 07:14:35 +0900

    出品→トップページへの遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, except: [:index]
+
   def index
     @items = Item.all
   end
@@ -20,6 +22,14 @@ class ItemsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 
 end
