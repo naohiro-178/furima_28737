24e2862ed303c20e1c4009d724d84f06d8f47d1c
naohiro-178 <69035007+naohiro-178@users.noreply.github.com>
2020-09-04 09:48:41 +0900

    バリデーション(仮)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -34,5 +34,6 @@ class ItemsController < ApplicationController
 
   def message_params
     params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
+  end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,16 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :explanation
+    validates :category_id 
+    validates :condition
+    validates :shipping_fee
+    validates :shipping_area
+    validates :shipping_date
+    validates :price
+  end
 end
