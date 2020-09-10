b2a63f33ffe61c24b973cca6c9f0a6e229794019
naohiro-178 <69035007+naohiro-178@users.noreply.github.com>
2020-09-10 18:54:52 +0900

    FactoryBot追記
    use_idに関するエラーの解消

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -8,7 +8,7 @@ FactoryBot.define do
     shipping_area {2}
     shipping_date {2}
     price {1000}
-
+    association :user
     after(:build) do |item|
       item.image.attach(io: File.open('public/images/test_image.png'),filename: 'test_image.png')
     end
