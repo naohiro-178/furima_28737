0e7dfc6d3414b0f60bbe7682322f32d1f1bb2cf2
naohiro-178 <69035007+naohiro-178@users.noreply.github.com>
2020-09-16 16:51:15 +0900

    商品情報編集機能(修正１回目)
    before_actionを使用し、edit,showアクション内の同一コードをまとめて記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
+  before_action :set_item, only: [:edit, :show]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -20,11 +21,9 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
@@ -40,5 +39,9 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:name, :image, :explanation, :category_id, :bland_id, :shipping_id, :shipping_area_id, :shipping_date_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
