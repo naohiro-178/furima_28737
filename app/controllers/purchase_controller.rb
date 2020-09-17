958ed6626d54e93e400791e90208247203d8a5e2
naohiro-178 <69035007+naohiro-178@users.noreply.github.com>
2020-09-17 18:06:38 +0900

    purchasesコントローラーの作成
    以前にpurchaseと単数形で作成してしまっていたため、複数形に名前を再定義して作成。

--- app/controllers/purchase_controller.rb
+++
@@ -1,4 +0,0 @@
-class PurchaseController < ApplicationController
-  def index
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   root "items#index"
   resources :items
   resources :users
-  resources :purchase, only:[:index, :new, :create]
+  resources :purchases, only:[:index, :new, :create]
 end
