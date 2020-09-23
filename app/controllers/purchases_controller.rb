c267c052d5f000e2feaa4baaef733a6bb91da5e1
naohiro-178 <69035007+naohiro-178@users.noreply.github.com>
2020-09-23 18:07:29 +0900

    SOLDOUTの実装・詳細＆購入ページの実装
    SOLDOUTの記述、購入後に編集や削除ができないこと、出品者は購入ページに入れないこと、購入済の商品は購入ページに飛べないこと

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,4 +1,7 @@
 class PurchasesController < ApplicationController
+  before_action :move_to_sign_in, only: [:index]
+  before_action :set_item, only: [:index, :create]
+  before_action :move_to_root, only: [:index]
   def index
     @items = Item.find(params[:item_id])
     @purchase = Purchase.find(params[:item_id])
@@ -21,6 +24,24 @@ class PurchasesController < ApplicationController
   end
 
   private
+
+  def move_to_sign_in
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def move_to_root
+    if user_signed_in? && current_user.id == @item.user_id
+      redirect_to root_path
+    elsif @item.purchase != nil
+      redirect_to root_path
+    end
+  end
 
   def purchase_params
     params.permit(:postal_code, :shipping_area_id, :municipalities, :address, :building, :phone_number, :item_id, :token).merge(user_id: current_user.id)
